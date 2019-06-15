908369c22907067efbd7e3cd552b7c8646f4b675
xiaoli <836075337@qq.com>
2019-06-15 13:30:43 +0800

    kalalala

--- ceshi_git/src/it/heima/test1.java
+++ ceshi_git/src/it/heima/test1.java
@@ -2,9 +2,7 @@ package it.heima;
 
 public class test1 {
     public static void main(String[] args) {
-        System.out.println("11111111");
-        System.out.println("22222222");
-        System.out.println("33333333");
+
         System.out.println("8888888888888");
     }
 }
