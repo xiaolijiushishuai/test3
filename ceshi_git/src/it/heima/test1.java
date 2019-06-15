1b6c4bd25c5df62dfd244d51fe1ff7d1f53f592a
xiaoli <836075337@qq.com>
2019-06-15 13:26:43 +0800

    小小测试

--- ceshi_git/src/it/heima/test1.java
+++ ceshi_git/src/it/heima/test1.java
@@ -5,5 +5,6 @@ public class test1 {
         System.out.println("11111111");
         System.out.println("22222222");
         System.out.println("33333333");
+        System.out.println("8888888888888");
     }
 }
